0c66bb8c57ad59746c6fc1f969e98c1f6cdf317d
Akshit Shetty <shettakshit119@gmail.com>
2024-04-16 01:05:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "end-to-end-ml-project-with-mlflow"
+AUTHOR_USER_NAME = "akki-119"
+SRC_REPO = "MLProject"
+AUTHOR_EMAIL = "shettyakshit119@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="ML Portfolio Project!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+
+)
